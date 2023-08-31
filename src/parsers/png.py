0c786f14b7cd008daf324793d1f14d4b82c3f9ff
MaikoVoigt <maikovoigt@gmail.com>
2023-08-31 11:43:00 +0200

    test

--- src/parsers/png.py
+++ src/parsers/png.py
@@ -1,6 +1,6 @@
 from src.interfaces import AnswerParser
 
 class PngParser(AnswerParser):
-    pass # TODO good luck ;-)
+    pass # TODO good luck ;-) (keine Lust)
 
 AnswerParser.register(PngParser)
