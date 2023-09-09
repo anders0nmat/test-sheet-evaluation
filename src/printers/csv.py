f9559668d25644732fb9900a0b65fa9c9a6dbcd4
Marq102 <schlau_dumm@web.de>
2023-09-09 17:51:39 +0200

    beide Printer

--- src/printers/csv.py
+++ src/printers/csv.py
@@ -1,6 +1,16 @@
+import csv
 from src.interfaces import FilePrinter
 
 class CsvPrinter(FilePrinter):
-    pass # TODO
+    def printStatistics(self):
+        for sheet in self.statBook.sheets:
+            sheet_name = sheet.name
+            sheet_data = sheet.rows
+            sheet_filename = f"{self.filePath.stem}_{sheet_name}.csv"
+
+            with open(sheet_filename, 'w', newline='', encoding='utf-8') as csvfile:
+                csv_writer = csv.writer(csvfile, delimiter=',')
+                for row in sheet_data:
+                    csv_writer.writerow(row.values)
 
 FilePrinter.register(CsvPrinter)
--- src/printers/excel.py
+++ src/printers/excel.py
@@ -1,6 +1,15 @@
 from src.interfaces import FilePrinter
+from openpyxl import Workbook
 
 class ExcelPrinter(FilePrinter):
-    pass # TODO use openpyxl
+    def printStatistics(self):
+        workbook = Workbook()
+        for sheet in self.statBook.sheets:
+            worksheet = workbook.create_sheet(title=sheet.name)
+            for row in sheet.rows:
+                worksheet.append(row.values)
+        
+        workbook.remove(workbook.active)    
+        workbook.save(self.filePath)        
 
 FilePrinter.register(ExcelPrinter)
