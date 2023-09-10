ac8d9fc2a363c70fb23f5091d02344100a4e5a0b
MaikoVoigt <maikovoigt@gmail.com>
2023-09-10 15:46:22 +0200

    pdf

--- src/parsers/pdf.py
+++ src/parsers/pdf.py
@@ -1,4 +1,10 @@
 from interfaces import AnswerParser
+from containers import Student
+from pathlib import Path
+import tempfile
+from parsers.png import PngParser
+
+
 from pdf2image import convert_from_path
 
 # (sudo apt install python3-pip) falls pip nicht installiert 
@@ -7,14 +13,35 @@ from pdf2image import convert_from_path
 
 class PdfParser(AnswerParser):
     # TODO maybe convert to png and hand off to PngParser
+    pdfFile: Path
+    def __init__(self, args: list[str]) -> None:
+        super().__init__()
+        self.pdfFile = Path(args[0])
 
-    def pdf_to_png(pdf_file, output_folder):
-
-
+    def pdf_to_png(self, pdf_file, output_folder) -> int:
         # Seiten des PDFs in PNG-Bilder konvertieren
-        images = convert_from_path(pdf_file, dpi=200, first_page=page_num + 1, last_page=page_num + 1)
+        images = convert_from_path(pdf_file, dpi=200)
 
         for i, image in enumerate(images):
-            image.save(f"{output_folder}/page_{page_num + 1}_{i + 1}.png", "PNG")
+            image.save(f"{output_folder}/{i + 1}.png", "PNG")
+        
+        return int(len(images))
+    
+    def extractAnswers(self) -> list[Student]:
+
+        with tempfile.TemporaryDirectory() as temp_directory:
+            num_Students = self.pdf_to_png(self.pdfFile, temp_directory)
+
+            result = list[Student]
+
+            for i in range(num_Students):
+                png_file :Path
+                png_file = f"{temp_directory}/{i + 1}.png"
+
+                result += PngParser.extractAnswers(png_file)
+
+
+
+        return result
 
 AnswerParser.register(PdfParser)
