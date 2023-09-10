8c4c15778f5152cc3024de7fee77b7dfa544bfe5
anders0nmat <fullgamefun@gmail.com>
2023-09-10 12:14:58 +0200

    canPrint, CSV respects user folder choice

--- src/printers/csv.py
+++ src/printers/csv.py
@@ -1,16 +1,21 @@
 import csv
 from src.interfaces import FilePrinter
+from pathlib import Path
 
 class CsvPrinter(FilePrinter):
-    def printStatistics(self):
-        for sheet in self.statBook.sheets:
-            sheet_name = sheet.name
-            sheet_data = sheet.rows
-            sheet_filename = f"{self.filePath.stem}_{sheet_name}.csv"
+	def printStatistics(self):
+		for sheet in self.statBook.sheets:
+			sheet_name = sheet.name
+			sheet_data = sheet.rows
+			sheet_filename = f"{self.filePath.stem}_{sheet_name}.csv"
 
-            with open(sheet_filename, 'w', newline='', encoding='utf-8') as csvfile:
-                csv_writer = csv.writer(csvfile, delimiter=',')
-                for row in sheet_data:
-                    csv_writer.writerow(row.values)
+			with open(self.filePath.with_name(sheet_filename), 'w', newline='', encoding='utf-8') as csvfile:
+				csv_writer = csv.writer(csvfile, delimiter=',')
+				for row in sheet_data:
+					csv_writer.writerow(row.values)
+	
+	@classmethod
+	def canPrint(cls, path: Path, book) -> bool:
+		return path.suffix.casefold() == '.csv'
 
 FilePrinter.register(CsvPrinter)
--- src/printers/excel.py
+++ src/printers/excel.py
@@ -1,15 +1,20 @@
 from src.interfaces import FilePrinter
 from openpyxl import Workbook
+from pathlib import Path
 
 class ExcelPrinter(FilePrinter):
-    def printStatistics(self):
-        workbook = Workbook()
-        for sheet in self.statBook.sheets:
-            worksheet = workbook.create_sheet(title=sheet.name)
-            for row in sheet.rows:
-                worksheet.append(row.values)
-        
-        workbook.remove(workbook.active)    
-        workbook.save(self.filePath)        
+	def printStatistics(self):
+		workbook = Workbook()
+		for sheet in self.statBook.sheets:
+			worksheet = workbook.create_sheet(title=sheet.name)
+			for row in sheet.rows:
+				worksheet.append(row.values)
+		
+		workbook.remove(workbook.active)    
+		workbook.save(self.filePath)
+
+	@classmethod
+	def canPrint(cls, path: Path, book) -> bool:
+		return path.suffix.casefold() in {'.xlsx', '.xls'}
 
 FilePrinter.register(ExcelPrinter)
